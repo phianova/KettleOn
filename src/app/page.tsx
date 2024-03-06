4a8b8dcd987a319ce5d716c5b25a69e049073618
Philliphick <81019680+Philliphick@users.noreply.github.com>
2024-03-06 12:33:19 +0000

    started config of aceternity components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
-import Image from "next/image";
-import { serverClient } from "../trpc/server-client";
+// import Image from "next/image";
+// import { serverClient } from "../trpc/server-client";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+import {useState, useEffect} from "react";
 
 
 export default async function Home() {
-  const connected = await serverClient.apiTest();
+  // const connected = await serverClient.apiTest();
+  const [content, setContent] = useState({})
+
+  useEffect(() => {
+    const test = {
+      title: "title",
+      quote: "description",
+      name: "image"
+      
+    }
+    setContent(test)
+  }, [])
+  
+  
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+   <><main>
+    
+
         <h1>Main Page</h1>
-        <p>{connected}</p>
-    </main>
+        <InfiniteMovingCards items={content} className={undefined}/>
+        {/* <p>{connected}</p> */}
+
+    </main> </>
   );
 }
