48460117facd170733e518ed6e61125f76ac75dd
phianova <98988580+phianova@users.noreply.github.com>
2024-03-11 10:12:56 +0000

    remove dead code

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-import { notFound, redirect } from "next/navigation";
 
 const page = () => {
 
-    // const {isAuthenticated, user, permissions} = useKindeBrowserClient();
-    // const currentUserData = user
-    // if (!isAuthenticated) redirect(`/auth-callback?origin=home`);
-  return (
+    return (
     <div>Home Page</div>
   )
 }
