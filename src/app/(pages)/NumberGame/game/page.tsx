1b4980a4c105ac29a2c52669c90a9d081ba9a93a
phianova <98988580+phianova@users.noreply.github.com>
2024-03-15 15:54:59 +0000

    type errors

--- src/app/(pages)/NumberGame/game/page.tsx
+++ src/app/(pages)/NumberGame/game/page.tsx
@@ -125,7 +125,7 @@ const [equation, setEquation] = useState("");
 
 
 
-const handleKeyPress = (number) => {
+const handleKeyPress = (number : number) => {
   setInputValue((prevValue) => prevValue + number);
   setEquation((prevEquation) => prevEquation + number);
   console.log(equation);
@@ -137,7 +137,7 @@ const handleClear = () => {
   setEquation("");
 };
 
-const handleMathSymbolClick = (symbol) => {
+const handleMathSymbolClick = (symbol : string) => {
   if(symbol !== "=") {
     setInputValue((prevValue) => prevValue + symbol);
     setEquation((prevEquation) => prevEquation + symbol);
@@ -160,7 +160,7 @@ const handleEquation = () => {
   resultCheck(result);
 }
 
-const resultCheck = (result) => {
+const resultCheck = (result : number) => {
   if (target === result) {
     setWin(true);
     console.log("correct");
