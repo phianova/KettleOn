74388e343dae5bcf708835d5af1293751f4d1bc2
Philliphick <81019680+Philliphick@users.noreply.github.com>
2024-05-09 12:30:46 +0100

    test cron vercel2

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -125,11 +125,11 @@ export default function Home() {
   useEffect(() => {
     console.log("inside useeffect")
      // node-scheduler that runs at midnight
-  schedule.scheduleJob('*/15 * * * *', function() {
+  schedule.scheduleJob('36 12 * * *', function() {
     console.log('24 hrs passed');
     teamUsageReset()
 });
-  schedule.scheduleJob('*/15 * * * *', function() {
+  schedule.scheduleJob('36 12 * * *', function() {
     console.log('1 week passed');
     teamScoreReset();
   })
