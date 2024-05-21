96030660f07c5b231afc41d86e76d4e94a21dae1
Philliphick <81019680+Philliphick@users.noreply.github.com>
2024-05-21 09:02:22 +0100

    fix async on usage

--- src/app/api/usage.js
+++ src/app/api/usage.js
@@ -1,12 +1,12 @@
 import { trpc } from "../../_trpc/client";
 export const dynamic = 'force-dynamic';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     const { mutate: teamUsageReset } = trpc.teamUsageReset.useMutation();
     console.log('Usage endpoint called by cron job');
     
     try {
-        teamUsageReset();
+        await teamUsageReset();
         res.status(200).json({ message: 'Usage data reset successfully' });
     } 
     catch (error) {
